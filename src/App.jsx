a26ef1082d9eb106e0a950e83226e59b7370b34f
BrianBSP <167891999+BrianBSP@users.noreply.github.com>
2024-09-27 14:04:35 +0200

    aggiunta fetch register

--- src/App.jsx
+++ src/App.jsx
@@ -1,26 +1,24 @@
-import './assets/Components/Css/App.css'
-import { BrowserRouter,Route, Routes } from 'react-router-dom'
-import Login from './assets/Components/React/Login.jsx'
-import Registrazione from './assets/Components/React/Registrazione.jsx'
-import Cliente from './assets/Components/React/Cliente.jsx'
-import Selezione from './assets/Components/React/Selezione.jsx'
-import Fattura from '../src/assets/Components/React/Fattura.jsx'
-import DashBoard from './assets/Components/React/DashBoard.jsx'
+import "./assets/Components/Css/App.css";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Login from "./assets/Components/React/Login.jsx";
+import Registrazione from "./assets/Components/React/Registrazione.jsx";
+import Cliente from "./assets/Components/React/Cliente.jsx";
+import Selezione from "./assets/Components/React/Selezione.jsx";
+import Fattura from "../src/assets/Components/React/Fattura.jsx";
+import DashBoard from "./assets/Components/React/DashBoard.jsx";
 
 function App() {
-
   return (
-    
-       <BrowserRouter>
-          <Routes>
-            <Route path="/" element={<Registrazione/>}/>
-            <Route path="/login" element={<Login/>}/>
-            <Route path="/selezione" element={<Selezione/>}/>
-            <Route path="/cliente" element={<Cliente/>}/>
-            <Route path="/fattura" element={<Fattura/>}/>
-            <Route path="/dashboard" element={<DashBoard/>}/>
-          </Routes>                                   
-      </BrowserRouter>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/auth/register" element={<Registrazione />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/selezione" element={<Selezione />} />
+        <Route path="/cliente" element={<Cliente />} />
+        <Route path="/fattura" element={<Fattura />} />
+        <Route path="/dashboard" element={<DashBoard />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
--- src/assets/Components/React/Registrazione.jsx
+++ src/assets/Components/React/Registrazione.jsx
@@ -1,70 +1,125 @@
-import {Col, Form, InputGroup, Row } from "react-bootstrap";
-import { Link } from "react-router-dom";
+import { useState } from "react";
+import { Col, Form, InputGroup, Row } from "react-bootstrap";
 
 const Registrazione = () => {
-    return(
-        <div>
-            <Form>
+  const [nome, setNome] = useState("");
+  const [cognome, setCognome] = useState("");
+  const [username, setUsername] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const handleRegister = async (e) => {
+    e.preventDefault();
+
+    const utente = { nome, cognome, username, email, password };
+
+    try {
+      const resp = await fetch(`http://localhost:3001/auth/register`, {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(utente),
+      });
+
+      if (resp.ok) {
+        const data = await resp.json();
+        console.log(data);
+        alert("Utente registrato con successo.");
+      } else {
+        alert("Registrazione fallita.");
+      }
+    } catch (error) {
+      console.error("Errore: ", error);
+      alert("Errore durante la registrazione");
+    }
+  };
+
+  return (
+    <div>
+      <Form onSubmit={handleRegister}>
         <h1 className="h1Login">EFFETTUA LA REGISTRAZIONE</h1>
 
         <div className="unione">
+          <InputGroup className="mb-3 emailFormCliente">
+            <InputGroup.Text id="basic-addon1" className="testo">
+              Nome
+            </InputGroup.Text>
+            <Form.Control
+              placeholder="Nome"
+              aria-label="Nome"
+              aria-describedby="basic-addon1"
+              className="formTextCliente"
+              value={nome}
+              onChange={(e) => setNome(e.target.value)}
+            />
+          </InputGroup>
 
-<InputGroup className="mb-3 emailFormCliente">
-    <InputGroup.Text id="basic-addon1" className="testo">Nome</InputGroup.Text>
-    <Form.Control
-      placeholder="Nome"
-      aria-label="Nome"
-      aria-describedby="basic-addon1"
-      className="formTextCliente"
-    />
-  </InputGroup>
-
-  <InputGroup className="mb-3 emailFormCliente">
-    <InputGroup.Text id="basic-addon1" className="testo">Cognome</InputGroup.Text>
-    <Form.Control
-      placeholder="Cognome"
-      aria-label="Cognome"
-      aria-describedby="basic-addon1"
-      className="formTextCliente"
-    />
-  </InputGroup>
-
-</div>
-<InputGroup className="mb-3 emailFormCliente">
-    <InputGroup.Text id="basic-addon1" className="testo" autoComplete="username">Username</InputGroup.Text>
-    <Form.Control
-      placeholder="Username"
-      aria-label="Username"
-      aria-describedby="basic-addon1"
-      className="formTextCliente"
-    />
-  </InputGroup>
-
-<div className="unione">
-
-        <Form.Group className="mb-3 emailFormReg " controlId="exampleForm.ControlInput1">
-        <Form.Label className="testo">Email</Form.Label>
-        <Form.Control type="email" placeholder="Inserisci la tua email" className="formTextReg" autoComplete="username" />
-        </Form.Group>
-        <Form.Group as={Row} className="mb-3 pwFormReg d-flex" controlId="formPlaintextPassword">
-        <Form.Label column sm="4" className="testo">
-            Password
-        </Form.Label>
-        <Col sm="10">
-          <Form.Control type="password" placeholder="Inserisci la tua email" className="formTextReg" autoComplete="current-password"/>
-        </Col>
-      </Form.Group>
-      
-</div>
-
-      <div className="ContBtnReg"> 
-      <Link to="/login" className="nav-link">
-         <button className="btnReg">REGISTRATI</button>
-      </Link>
-      </div>
-      </Form>
+          <InputGroup className="mb-3 emailFormCliente">
+            <InputGroup.Text id="basic-addon1" className="testo">
+              Cognome
+            </InputGroup.Text>
+            <Form.Control
+              placeholder="Cognome"
+              aria-label="Cognome"
+              aria-describedby="basic-addon1"
+              className="formTextCliente"
+              value={cognome}
+              onChange={(e) => setCognome(e.target.value)}
+            />
+          </InputGroup>
         </div>
-    )
-}
+        <InputGroup className="mb-3 emailFormCliente">
+          <InputGroup.Text id="basic-addon1" className="testo" autoComplete="username">
+            Username
+          </InputGroup.Text>
+          <Form.Control
+            placeholder="Username"
+            aria-label="Username"
+            aria-describedby="basic-addon1"
+            className="formTextCliente"
+            value={username}
+            onChange={(e) => setUsername(e.target.value)}
+          />
+        </InputGroup>
+
+        <div className="unione">
+          <Form.Group className="mb-3 emailFormReg " controlId="exampleForm.ControlInput1">
+            <Form.Label className="testo">Email</Form.Label>
+            <Form.Control
+              type="email"
+              placeholder="Inserisci la tua email"
+              className="formTextReg"
+              autoComplete="username"
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+            />
+          </Form.Group>
+          <Form.Group as={Row} className="mb-3 pwFormReg d-flex" controlId="formPlaintextPassword">
+            <Form.Label column sm="4" className="testo">
+              Password
+            </Form.Label>
+            <Col sm="10">
+              <Form.Control
+                type="password"
+                placeholder="Inserisci la tua password"
+                className="formTextReg"
+                autoComplete="current-password"
+                value={password}
+                onChange={(e) => setPassword(e.target.value)}
+              />
+            </Col>
+          </Form.Group>
+        </div>
+
+        <div className="ContBtnReg">
+          <button type="submit" className="btnReg">
+            REGISTRATI
+          </button>
+        </div>
+      </Form>
+    </div>
+  );
+};
 
 export default Registrazione;
